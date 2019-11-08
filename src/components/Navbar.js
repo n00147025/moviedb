d0dde45b576c260b2e6686ea731dab911aba057e
n00147025 <44161672+n00147025@users.noreply.github.com>
2019-11-08 11:20:04 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 /**
  * @Date:   2019-11-06T23:02:43+00:00
- * @Last modified time: 2019-11-08T00:13:16+00:00
+ * @Last modified time: 2019-11-08T11:19:51+00:00
  */
 import React from 'react';
 
-import {Link} from "react-router-dom";
+import {NavLink,Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -14,17 +14,17 @@ const Navbar = () => {
           <img src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" style={{paddingRight: 10}}/>
           Movie DB
         </a>
-          <ul className="navbar-nav">
-            <Link to="/About">
-            <li  className="nav-link">
-              About
-            </li>
-            </Link>
-            <Link to="/Home" >
-            <li className="nav-link">
-              Home
-            </li>
-            </Link>
+          <ul className="navbar-nav inline" style={{display: 'inline'}}>
+            <NavLink to="/About" style={{textDecoration: 'none'}}>
+              <li className="nav-link">
+                About
+              </li>
+            </NavLink>
+            <NavLink to="/Home" >
+              <li className="nav-link" style={{textDecoration: 'none'}}>
+                Home
+              </li>
+            </NavLink>
           </ul>
       </div>
     </nav>
